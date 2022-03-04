ccc7d9b6dfec651c9ad338ce1a3c6400e0b2b9f1
= <2316683971@qq.com>
2022-03-04 11:18:00 +0800

    事件绑定

---
+++ pages/demo04/demo04.js
@@ -0,0 +1,29 @@
+// pages/demo04/demo04.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        num : 0
+    },
+    
+    //输入框的input事件的执行逻辑
+    handleInput(e){  //e 为事件源对象
+        // console.log(e.detail.value);
+        this.setData({
+            num : e.detail.value
+        })
+    },
+
+    //加 减 按钮的事件
+    handletap(e){
+        // console.log(e);
+        // 1 获取自定义属性 operation
+        const operation = e.currentTarget.dataset.operation;
+        this.setData({
+            num : this.data.num + operation
+        })
+    }
+
+})
