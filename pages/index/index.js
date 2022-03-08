3f12163cf943261171b62c7df59936bb21c37353
= <2316683971@qq.com>
2022-03-08 15:17:36 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,24 @@
 //Page Object
 Page({
   data: {
-    
+    //轮播图数组
+    swiperList:[]
   },
-  //options(Object)
+  //页面开始加载就会触发
   onLoad: function(options) {
-    
+      //1 发送异步请求获取轮播图数据
+      wx-wx.request({
+        url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+        enableCache: true,
+        enableHttp2: true,
+        enableQuic: true,
+        timeout: 0,
+        success: (result) => {
+          this.setData({
+            swiperList:result.data.message
+          })
+        },
+      })
   },
   onReady: function() {
     
